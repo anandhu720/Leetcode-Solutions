9665abf2365f5a8abcfd1738238bbc2fe79ad0a8
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-01-31 20:51:09 +0530

    Time: 5 ms (9.28%), Space: 6.9 MB (59.67%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.cpp
+++ 228-summary-ranges/228-summary-ranges.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
-        if(nums.size() == 0) return {};
-        vector<string> ans;
-        int start = nums[0];
-        for(int i=1;i<nums.size();i++){
-            if(nums[i-1]+1 != nums[i]){
-                if(nums[i-1] != start){
-                    string temp = to_string(start)+"->"+to_string(nums[i-1]);
-                    start = nums[i];
-                    ans.push_back(temp);
-                }else{
-                    ans.push_back(to_string(start));
-                    start = nums[i];
+        vector<string> v;
+        if(nums.size()==0){
+            return v;        
+        }
+        int start = nums[0], n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            if (i == n - 1 || nums[i] + 1 != nums[i + 1])
+            {
+                if (start == nums[i])
+                {
+                v.push_back(to_string(start));
                 }
+                else
+                {
+                    v.push_back(to_string(start) + "->" + to_string(nums[i]));
+                }
+                if (i < n - 1)
+                    start = nums[i + 1];
             }
         }
-        
-        if(start != nums[nums.size()-1]){
-            string temp = to_string(start)+"->"+to_string(nums[nums.size()-1]);
-            ans.push_back(temp);
-        }else{
-            ans.push_back(to_string(nums[nums.size()-1]));
-        }
-        
-        return ans;
+        return v;  
     }
 };
