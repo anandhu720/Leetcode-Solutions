6653a3170268a030d7332b1760121568d7b1543c
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-02-02 22:51:25 +0530

    Time: 240 ms (55.21%), Space: 10.5 MB (36.69%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    //Time = O(n*n)
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> dp(nums.size(),1);
+        for(int i=1;i<nums.size();i++){
+            int maxValue = 1;
+            for(int j=0;j<i;j++){
+                if(nums[j] < nums[i])
+                    maxValue = max(maxValue,dp[j]+dp[i]);
+            }
+            dp[i] = maxValue;
+        }
+        
+        int ans = INT_MIN;
+        for(auto it : dp)
+            ans = max(ans,it);
+        return ans;
+    }
+};
