c8bda5d0b3e5ec1789fc9a0df9f5ac6f670635c9
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-02-03 11:28:15 +0530

    Time: 475 ms (17.80%), Space: 24.5 MB (49.44%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.cpp
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
-        unordered_map<int,int> map1,map2;
-        int n = nums1.size();
+        unordered_map<int,int> mp;
         
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                map1[nums1[i]+nums2[j]]++;
-                map2[nums3[i]+nums4[j]]++;
+        for(auto i: nums1){
+            for(auto j: nums2){
+                mp[i+j]++;
+            }
+        }
+        int count = 0;
+        for(auto i: nums3){
+            for(auto j: nums4){
+                count += mp[-i-j];
             }
         }
         
-        int count=0;
-        for(auto it : map1){
-            int temp = it.first;
-            if(map2.find(-temp) != map2.end()) count += map2[-temp]*map1[temp];
-        }
         return count;
     }
 };
