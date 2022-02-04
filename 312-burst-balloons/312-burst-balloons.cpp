0ca1b4c805869f5f126c71248cad3d2584ad7f22
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-02-04 22:03:04 +0530

    Time: 492 ms (75.51%), Space: 10.4 MB (8.98%) - LeetHub

---
+++ 312-burst-balloons/312-burst-balloons.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        int n = nums.size()+2;
+        
+        //adding 1 in last and first of given array
+        int arr[n];
+        for(int i=1;i<n-1;i++) arr[i] = nums[i-1];
+        arr[0] = arr[n-1] = 1;
+        
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        
+        for(int len=0;len<n-1;len++){ //sliding window
+           for(int start=1;start<n-len;start++){
+               int end = start+len-1; //ending and starting point of sliding window
+               for(int k=start;k<=end;k++){ //taking one by obe element in sliding window
+                   dp[start][end] = max(dp[start][end],dp[start][k-1]+arr[k]*arr[start-1]*arr[end+1]+dp[k+1][end]);
+               }
+           } 
+        }
+       return dp[1][n-2]; 
+    }
+};
