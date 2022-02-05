feaa9d0f520dc6141aafb313ff23ac5aa1e82f4d
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-02-05 14:55:40 +0530

    Time: 28 ms (86.93%), Space: 89.3 MB (60.51%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
+    //Time = O(log(m+n))
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n = nums1.size()+nums2.size();
-        if(n == 0) return 0;
-        priority_queue<int,vector<int>,greater<int>> pq;
+        if(nums2.size() < nums1.size()) return findMedianSortedArrays(nums2,nums1);
+        int n1 = nums1.size();
+        int n2 = nums2.size();
         
-        for(auto it : nums1) pq.push(it);
-        for(auto it : nums2) pq.push(it);
+        int low = 0,high = n1;
         
-        vector<int> ans(nums1.size()+nums2.size());
-        int i=0;
-        while(!pq.empty()){
-            ans[i] = pq.top();pq.pop();
-            i++;
+        while(low <= high){
+            int cut1 = low+(high-low)/2;
+            int cut2 = (n1+n2+1)/2 - cut1;
+            
+            int left1 = cut1 == 0 ? INT_MIN : nums1[cut1-1];
+            int left2 = cut2 == 0 ? INT_MIN : nums2[cut2-1];
+            
+            int right1 = cut1 == n1 ? INT_MAX : nums1[cut1];
+            int right2 = cut2 == n2 ? INT_MAX : nums2[cut2];
+            
+            if(left1 <= right2 && left2 <= right1){
+                if((n1+n2)%2 == 0){ //even
+                    return (max(left1,left2)+min(right1,right2))/2.0;
+                }else{ //odd
+                    return max(left1,left2);
+                }
+            }else if(left1 > right2){
+                high = cut1-1;
+            }else{
+                low = cut1+1;
+            }
         }
-        
-        
-        
-        if(n%2 != 0){
-            return (double)ans[n/2];
-        }
-        
-        n /= 2;
-        cout<<ans[n]<<" "<<ans[n-1]<<endl;
-        double answer = ans[n]+ans[n-1];
-        answer /= 2;
-        return answer;
+        return 0.0;
     }
 };
