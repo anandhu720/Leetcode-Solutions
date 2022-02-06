39615a42a9535b5f6f8d0ca9bb981e5085d4b108
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-02-06 22:35:15 +0530

    Time: 704 ms (5.02%), Space: 16.2 MB (12.74%) - LeetHub

---
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool dfs(int i,int j,string &s,string &p,map<pair<int,int>,bool> &cache){
+        
+        if(cache[{i,j}]) return cache[{i,j}];
+        
+        if(i >= s.length() && j >= p.length()) return true;
+        if(j >= p.length()) return false;
+        
+        bool match = false;
+        if(i < s.length() && (s[i] == p[j] || p[j] == '.')) match = true;
+        
+        if(j+1 < p.length() && p[j+1] == '*'){ //here we have 2 choices
+            return cache[{i,j}] =  (dfs(i,j+2,s,p,cache) ||  //dont use *
+                    (match && dfs(i+1,j,s,p,cache)));  //use * 
+        }
+        if(match) return cache[{i,j}] =  dfs(i+1,j+1,s,p,cache);
+        return cache[{i,j}] = false;
+            
+    }
+    bool isMatch(string s, string p) {
+        map<pair<int,int>,bool> cache;
+        return dfs(0,0,s,p,cache);
+    }
+};
