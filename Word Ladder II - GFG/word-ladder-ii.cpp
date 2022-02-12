90e79571f11176c939ef2ba5ad83f7c6de703d57
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-02-12 21:04:53 +0530

    Added solution - LeetHub

--- Word Ladder II - GFG/word-ladder-ii.cpp
+++ Word Ladder II - GFG/word-ladder-ii.cpp
@@ -24,47 +24,42 @@ public:
     }
 
     vector<vector<string>> findSequences(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_map<string, unordered_set<string>> adj;   //Adjacency List
-        unordered_set<string> dict(wordList.begin(),wordList.end());   //Insert WordList in SET
+        unordered_map<string,unordered_set<string>> adj;
+        unordered_set<string> myset(wordList.begin(),wordList.end());
         
-        //STEP-1: Find min-depth using BFS
-        queue<string> Q;    //For BFS traversal
-        Q.push(beginWord);  //Push start node (beginWord)
-        unordered_map<string, int> visited; //Key->String (Node)...Value->Level (Depth of traversal)
-        visited[beginWord] = 0; //Start node will always be at level 0
-        while(!Q.empty())
-        {
-            string curr = Q.front();
-            Q.pop();
+        queue<string> q;
+        q.push(beginWord);
+        
+        unordered_map<string,int> visited; //<string,depth>
+        visited[beginWord] = 0;
+        while(!q.empty()){
+            string curr = q.front(); q.pop();
             string temp = curr;
-            for(int i = 0; i < curr.size(); ++i)    //For all characters
-            {
-                for(char x = 'a'; x <= 'z'; ++x)    //Try all possible 26 letters
-                {
-                    if(temp[i] == x)    //Skip if letter is same as in original word
-                        continue;
-
-                    temp[i] = x;    
-                    if(dict.count(temp) > 0)    //Check if new word is present in wordList
-                    {
-                        if(visited.count(temp) == 0)    //check if the new word was already visited
-                        {
+            for(int i=0;i<curr.size();++i){
+                for(char c='a';c<='z';++c){
+                    if(temp[i] == c) continue;
+                    
+                    temp[i] = c;
+                    
+                    if(myset.count(temp) > 0){
+                        if(visited.count(temp) == 0){
                             visited[temp] = visited[curr] + 1;
-                            Q.push(temp);
+                            q.push(temp);
                             adj[curr].insert(temp);
-                        } 
-                        else if(visited[temp] == visited[curr] + 1) //If already visited and new word is the child (We should always move down)
+                        }else if(visited[temp] == 1+visited[curr]){
                             adj[curr].insert(temp);
+                        }
                     }
                 }
-                temp[i] = curr[i];  //Revert back temp to curr
+                temp[i] = curr[i];
             }
         }
-        //STEP-2: Find all possible paths at min-depth using DFS
+        
         vector<string> path;
         vector<vector<string>> ans;
-        dfs(beginWord, endWord, adj, path,ans); //Find all possible paths with min-depth
-        return ans; 
+        dfs(beginWord,endWord,adj,path,ans);
+        return ans;
+        
     }
 };
 
