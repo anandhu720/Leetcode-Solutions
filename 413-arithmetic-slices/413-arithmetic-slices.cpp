b19a3870b1f278e4a2060ce65f44d5589b58573a
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-02-15 21:22:52 +0530

    Time: 7 ms (17.99%), Space: 7.3 MB (87.62%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -1,28 +1,19 @@
 class Solution {
 public:
     int numberOfArithmeticSlices(vector<int>& nums) {
-        vector<vector<pair<int,int>>> dp(nums.size(),vector<pair<int,int>>(nums.size(),{0,0}));
-        
-        for(int g=0;g<nums.size();g++){
-            int i,j;
-            for(i=0,j=g;j<nums.size();j++,i++){
-                if(g==0)
-                    dp[i][j] = {0,0};
-                else if(g==1)
-                    dp[i][j] = {0,nums[j]-nums[j-1]};
-                else{
-                    if(nums[j] - nums[j-1] == dp[i][j-1].second)
-                        dp[i][j] = {dp[i][j-1].first+1,nums[j] - nums[j-1]};
-                    else
-                        dp[i][j] = {dp[i][j-1].first,INT_MAX};
-                }
+        int solve = 0;
+        int diff = 0;
+        int from = 0;
+        for (int to = 1; to < nums.size(); to++) {
+            int localDiff = nums[to] - nums[to - 1];
+            if (localDiff != diff) {
+                from = to - 1;
+                diff = localDiff;
+            }
+            if (to - from + 1 >= 3) {
+                solve += to - from - 1;
             }
         }
-        
-        int ans = 0;
-        for(int i=0;i<nums.size();i++)
-            ans+=dp[i][nums.size()-1].first;
-            
-        return ans;
+        return solve;
     }
 };
