ce16e85f5103bd21146439142c4f8f8e9efdaa25
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-02-16 21:34:09 +0530

    Time: 21 ms (43.37%), Space: 9.8 MB (93.02%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
-        vector<vector<int>> dp = matrix;
-        for(int i=dp.size()-2;i>=0;i--){
-            for(int j=0;j<dp[i].size();j++){
+        for(int i=matrix.size()-2;i>=0;i--){
+            for(int j=0;j<matrix[i].size();j++){
                 if(j == 0){
-                    dp[i][j] += min(dp[i+1][j],dp[i+1][j+1]);
-                }else if(j == dp[i].size()-1){
-                    dp[i][j] += min(dp[i+1][j],dp[i+1][j-1]);
+                    matrix[i][j] += min(matrix[i+1][j],matrix[i+1][j+1]);
+                }else if(j == matrix[i].size()-1){
+                    matrix[i][j] += min(matrix[i+1][j],matrix[i+1][j-1]);
                 }else{
-                    dp[i][j] += min(dp[i+1][j],min(dp[i+1][j+1],dp[i+1][j-1]));
+                    matrix[i][j] += min(matrix[i+1][j],min(matrix[i+1][j+1],matrix[i+1][j-1]));
                 }
             }
         }
         
         int mini = INT_MAX;
-        for(int i=0;i<dp[0].size();i++)
-            mini = min(mini,dp[0][i]);
+        for(int i=0;i<matrix[0].size();i++)
+            mini = min(mini,matrix[0][i]);
         return mini;
     }
 };
