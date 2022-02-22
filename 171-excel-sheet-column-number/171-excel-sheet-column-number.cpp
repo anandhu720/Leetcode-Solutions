a2544d6314d972b40cc8607e3ef43920ddc7b114
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-02-22 10:05:08 +0530

    Time: 5 ms (22.87%), Space: 6.2 MB (13.67%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-    int titleToNumber(string columnTitle) {
-        unordered_map<char,int> hash;
-        int x = 1;
-        for(char c='A';c<='Z';c++) hash[c] = x++;
-        
-        string str = columnTitle;
-        x = 0;
+    int titleToNumber(string str) {
+        int x = 0;
         int ans = 0;
         
         for(int i=str.length()-1;i>=0;i--){
-            ans = ans + (pow(26,x) * hash[str[i]]);
+            int ascii = (int)str[i];
+            int value = ascii - 64;
+            cout<<value<<endl;
+            ans = ans + (pow(26,x) * value);
             x++;
         }
          
