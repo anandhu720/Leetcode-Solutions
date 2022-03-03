06e6f00b0f116f048697b01b2c40cf848f3785bc
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-03-03 08:39:07 +0530

    Time: 12 ms (92.86%), Space: 9.7 MB (43.85%) - LeetHub

---
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int start = 0;
+        for(int i=0;i<=s.length();i++){
+            if(s[i] == ' ' or i == s.length()){
+                cout<<start<<" "<<i<<endl;
+                reverse(s.begin()+start,s.begin()+i);
+                start = -1;
+            }
+            if(start == -1 and s[i] != ' ')
+                start = i;
+        }
+        
+        return s;
+    }
+};
