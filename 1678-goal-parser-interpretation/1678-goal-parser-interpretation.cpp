de5ddcf1b16e6ec2fa5f139efa23e289e318e2b5
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-03-08 11:46:22 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (14.33%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string interpret(string command) {
+        string ans = "";
+        int i=0;
+        while(i<command.length()) {
+            if(command[i] == 'G') {
+                ans += 'G';
+                i += 1;
+            }else if(command[i] == '(' and command[i+1] == ')') {
+                ans += 'o';
+                i += 2;
+            }else{
+                ans += "al";
+                i += 4;
+            }
+        }
+        return ans;
+    }
+};
