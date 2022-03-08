303b4d2e9ef785c06795e9eb161824a7cf801161
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-03-08 09:29:47 +0530

    Time: 0 ms (100.00%), Space: 8.9 MB (32.38%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,19 +1,32 @@
 class Solution {
 public:
+    void find(vector<int> &nums,unordered_map<int,int> &umap) {
+        stack<int> st;
+        st.push(0);
+        int i=1;
+        while(i < nums.size()){
+            if(st.empty() or nums[i] < nums[st.top()]){
+                st.push(i);
+                i++;
+            }else{
+                umap[nums[st.top()]] = nums[i];
+                st.pop();
+            }
+        }
+        
+        while(!st.empty()){
+            umap[nums[st.top()]] = -1;
+            st.pop();
+        }
+    }
+    
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int,int> umap;
+        find(nums2,umap);
+        
         vector<int> ans;
         for(int i=0;i<nums1.size();i++){
-            bool found = false;
-            for(int j=0;j<nums2.size();j++){
-                if(nums1[i] < nums2[j] and found == true){
-                    ans.push_back(nums2[j]);
-                    found = false;
-                    break;
-                }
-                if(nums1[i] == nums2[j]) found = true;
-            }
-            if(found == true)
-                ans.push_back(-1);
+            ans.push_back(umap[nums1[i]]);
         }
         
         return ans;
