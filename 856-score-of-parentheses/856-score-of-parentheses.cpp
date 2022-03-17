6ab96ade44f63e22fa7b2f6bda8c5baaccd5a032
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-03-17 13:50:15 +0530

    Time: 2 ms (37.38%), Space: 6.1 MB (45.05%) - LeetHub

---
+++ 856-score-of-parentheses/856-score-of-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int scoreOfParentheses(string s) {
+        stack<int> st;
+        int ans = 0;
+        
+        for(auto x : s) {
+            
+            if(x == '(') {
+               
+                st.push(ans);
+                ans = 0;
+                
+            }else {
+                
+                ans = st.top() + max(ans*2,1);
+                st.pop();
+                
+            }
+            
+        }
+        
+        return ans;
+    }
+};
