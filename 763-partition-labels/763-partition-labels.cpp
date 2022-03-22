8385288a2d795873ce6356f318b1336a6eae9e48
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-03-22 09:40:54 +0530

    Time: 11 ms (18.03%), Space: 6.7 MB (43.56%) - LeetHub

--- 763-partition-labels/763-partition-labels.cpp
+++ 763-partition-labels/763-partition-labels.cpp
@@ -1,54 +1,25 @@
-// class Solution {
-// public:
-//     vector<int> partitionLabels(string s) {
-//         vector<int> ans;
-//         vector<int> lastIndex(26);
-        
-//         for(int i=0;i<s.length();i++) {
-//             lastIndex[s[i] - 'a'] = i;
-//         }
-        
-        
-//         int i=0;
-//         while(i < s.length()) {
-//             int end = lastIndex[s[i] - 'a'];
-//             int j = i+1;
-//             while(j != end) {
-//                 end = max(end,lastIndex[s[j] - 'a']);
-//                 j++;
-//             }
-            
-//             ans.push_back(j - i + 1);
-//             i = j+1;
-//         }
-        
-//         return ans;
-        
-//     }
-// };
-
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        unordered_map<char,int>mp;
-        // filling impact of character's
-        for(int i = 0; i < s.size(); i++){
-            char ch = s[i];
-            mp[ch] = i;
-        }
-        // making of result
-        vector<int> res;
-        int prev = -1;
-        int maxi = 0;
+        unordered_map<char,int> map;
         
-        for(int i = 0; i < s.size(); i++){
-            maxi = max(maxi, mp[s[i]]);
-            if(maxi == i){
-                // partition time
-                res.push_back(maxi - prev);
-                prev = maxi;
+        for(int i=0;i<s.length();i++) {
+            map[s[i]] = i;
+        }
+        
+        int end = 0;
+        int prev = -1;
+        vector<int> ans;
+        
+        for(int i=0;i<s.length();i++) {
+            end = max(end,map[s[i]]);
+            if(end == i) {
+                ans.push_back(end - prev);
+                prev = end;
             }
         }
-        return res;
+        
+        return ans;
     }
 };
+
