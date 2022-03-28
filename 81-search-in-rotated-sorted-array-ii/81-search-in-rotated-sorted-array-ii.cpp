8516b59686777860bb98b119ef2eec3bd4eb8ede
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-03-28 20:42:54 +0530

    Time: 3 ms (95.97%), Space: 13.9 MB (90.12%) - LeetHub

--- 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.cpp
@@ -1,8 +1,27 @@
 class Solution {
 public:
+    //binary search
     bool search(vector<int>& nums, int target) {
-        for(int i=0;i<nums.size();i++) {
-            if(nums[i] == target) return true;
+       int lo = 0, ho = nums.size()-1;
+        while(lo <= ho){
+            int mid = lo+(ho-lo)/2;
+            if(nums[mid] == target) return true;
+            //here we have duplicates to avoid it
+            if((nums[lo] == nums[mid]) and (nums[ho] == nums[mid])) {
+                lo++;
+                ho--;
+            }
+            else if(nums[lo] <= nums[mid]){
+                
+                if(nums[mid] >= target && nums[lo] <= target) ho = mid -1;
+                else lo = mid+1;
+                
+            }else if (nums[mid] <= nums[ho]){
+                
+                if(nums[mid] <= target && nums[ho] >= target) lo = mid+1;
+                else ho = mid-1;
+                
+            }
         }
         return false;
     }
