6e40083ea3219fcc86c0525d93a8b5110476139e
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-03-28 22:29:46 +0530

    Time: 64 ms (49.41%), Space: 49.5 MB (6.99%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     bool canReach(vector<int>& arr, int start) {
-        unordered_map<int,bool> umap;
         unordered_set<int> visited;
-        return dfs(start,arr,umap,visited);
+        return dfs(start,arr,visited);
     }
     
-    bool dfs(int index,vector<int> &arr,unordered_map<int,bool> &umap,unordered_set<int> &visited) {
+    bool dfs(int index,vector<int> &arr,unordered_set<int> &visited) {
         
         if(index >= arr.size() or index < 0 or visited.count(index)) return false;
         if(arr[index] == 0) return true;
         
-        if(umap[index]) return umap[index];
         visited.insert(index);
         
-        bool add = dfs(index+arr[index],arr,umap,visited);
-        bool sub = dfs(index-arr[index],arr,umap,visited);
+        bool add = dfs(index+arr[index],arr,visited);
+        bool sub = dfs(index-arr[index],arr,visited);
         
-        return umap[index] = (add or sub);
+        return (add or sub);
     }
 };
