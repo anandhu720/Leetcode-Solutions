828ee5836cecdbbcba1d63e72f19ef019baec078
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-04-05 12:39:44 +0530

    Time: 7 ms (95.64%), Space: 11.5 MB (50.22%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -1,10 +1,15 @@
 class Solution {
 public:
-    int peakIndexInMountainArray(vector<int>& nums) {
-        for(int i=1;i<nums.size();i++) {
-            if(nums[i -1] > nums[i]) 
-                return i-1;
+    //Binary Search
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int left = 0,right = arr.size()-1;
+        while(left <= right) {
+            int mid = left + (right - left)/2;
+            
+            if(arr[mid] < arr[mid+1]) {
+                left = mid+1;
+            }else right = mid-1;
         }
-        return -1;
+        return left;
     }
 };
