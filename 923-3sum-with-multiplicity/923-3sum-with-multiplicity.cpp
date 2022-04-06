5cf4d119b76e9235c83b05d388c37ee4c4a903ce
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-04-06 20:22:18 +0530

    Time: 8 ms (97.62%), Space: 10.2 MB (98.30%) - LeetHub

--- 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
+++ 923-3sum-with-multiplicity/923-3sum-with-multiplicity.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     int threeSumMulti(vector<int>& arr, int target) {
-        int mod = 1000000007;
-        long result = 0;
-        for(int i=0;i<arr.size();i++) {
-            int count[101] = {0};
-            for(int j=i+1;j<arr.size();j++) {
-                int temp = target - arr[i] - arr[j];
-                if(temp >= 0 and temp <= 100 and count[temp] > 0) {
-                    result += count[temp];
-                    result %= mod;
+      	int mod = 1000000007;
+        long result = 0; 
+        long c[101] = {0};
+        for(int i : arr) c[i]++;
+        
+        for(int i = 0; i <= 100; i++) {
+            for(int j = i; j <= 100; j++) {
+                int k = target-i-j;
+                if(k<0 || k >100) continue;
+                if(i == j && j==k) {
+                    result+= (c[i] * (c[i]-1) * (c[i]-2) /6);
                 }
-                count[arr[j]]++;
+                else if(i == j && j != k) {
+                    result += ((c[i] * (c[i]-1) /2 ) * c[k]);
+                }
+                else if(i < j && j < k) {
+                    result += (c[i] * c[j] * c[k]);
+                }
             }
         }
-        return (int)result;
+        return (int)(result%mod);  
     }
 };
-
