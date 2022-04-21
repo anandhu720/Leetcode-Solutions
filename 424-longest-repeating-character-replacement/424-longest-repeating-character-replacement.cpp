01853988fbc5a703125f37cbb0b705f430df538d
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-04-21 21:51:16 +0530

    Time: 11 ms (73.18%), Space: 7.1 MB (13.13%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -3,14 +3,12 @@ public:
     int characterReplacement(string s, int k) {
         unordered_map<char,int> umap;
         int length = 0,l=0,r=0;
+        int maxi = 0;
         
         while(r < s.length()) {
             umap[s[r]]++;
             
-            int maxi = 0;
-            for(auto it : umap) {
-                maxi = max(maxi,it.second);
-            }
+            if(umap[s[r]] > maxi) maxi = umap[s[r]];
             
             while((r-l+1) - maxi > k) {
                 umap[s[l]]--;
