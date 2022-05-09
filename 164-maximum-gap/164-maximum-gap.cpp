63b5def662b20d2a3bf706e2b196e67bb1fa883b
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-05-09 12:23:41 +0530

    Time: 228 ms (60.06%), Space: 78.4 MB (42.63%) - LeetHub

--- 164-maximum-gap/164-maximum-gap.cpp
+++ 164-maximum-gap/164-maximum-gap.cpp
@@ -1,18 +1,41 @@
 class Solution {
 public:
     int maximumGap(vector<int>& nums) {
-        priority_queue<int> q;
-        for(auto it : nums) q.push(it);
-        
-        int i = q.top(); q.pop();
-        int gap = 0;
-        while(!q.empty()) {
-            int temp = i - q.top();
-            gap = max(gap,temp);
-            
-            i = q.top(); q.pop();
-        }
-        
-        return gap;
+        int mn = INT_MAX, mx = INT_MIN;
+        int n = nums.size();
+        if (n < 2)
+        	return 0;
+        for (int i=0;i<n;++i) {
+        	mn = min(nums[i], mn);
+        	mx = max(nums[i], mx);
+		}
+		if (mx == mn)
+			return 0;
+		double bucket = (mx - mn + 0.0) / n; 
+		vector<int> big(n, -1); 
+		vector<int> small(n, -1);  
+		for (int i=0;i<n;++i) {
+			int index = (nums[i] - mn) / bucket;
+			if (index >= n) 
+				index = n - 1; 
+			
+			if (big[index] == -1) {
+				big[index] = small[index] = nums[i];
+			}
+			else {
+				big[index] = max(big[index], nums[i]);
+				small[index] = min(small[index], nums[i]);
+			}
+		}
+		int result = 0;
+		int s = mn, b = mn;
+		for (int i=0;i<n;++i) { 
+			if (big[i] == -1)
+				continue;
+			result = max(small[i] - b, result); 
+			s = small[i];
+			b = big[i];
+		}
+		return result;
     }
 };
