bb45134034352ee90b82deb86b0c4b6d8574ecf3
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-05-27 10:05:34 +0530

    Time: 3 ms (43.94%), Space: 6.6 MB (37.91%) - LeetHub

--- 394-decode-string/394-decode-string.cpp
+++ 394-decode-string/394-decode-string.cpp
@@ -10,53 +10,31 @@ public:
  
         return s;
     }
-    
     string decodeString(string s) {
-        stack<char> st;
+        stack<string> st;
+        string currString = "";
+        int currNum = 0;
         
         for(int i=0;i<s.length();i++) {
             
-            if(s[i] != ']') st.push(s[i]);
-            else{
-                
-                string temp = "";
-                while(!st.empty() and st.top() != '[') {
-                    temp.push_back(st.top());
-                    st.pop();
-                }
-                
-                reverse(temp.begin(),temp.end());
-                
-                st.pop();
-                
-                string x = "";
-                while(!st.empty() and st.top() >= '0' and st.top() <= '9'){
-                    x.push_back(st.top());
-                    st.pop();
-                }
-                
-                reverse(x.begin(),x.end());
-                
-                int num = stoi(x);
-                
-                string ans = repeat(temp,num);
-                
-                for(auto it : ans) st.push(it);
-                
-                
+            if(s[i] == '[') {
+                st.push(currString);
+                st.push(to_string(currNum));
+                currNum = 0;
+                currString = "";
+            }else if(s[i] == ']') {
+                int num = stoi(st.top()); st.pop();
+                string prevString = st.top(); st.pop();
+                currString = prevString + repeat(currString,num);
+            }else if(s[i] >= '0' and s[i] <= '9') {
+                currNum = currNum*10 + (s[i] - 48);
+            }else{
+                currString.push_back(s[i]);
             }
             
         }
         
-        string ans = "";
-        while(!st.empty()) {
-            ans.push_back(st.top());
-            st.pop();
-        }
         
-        reverse(ans.begin(),ans.end());
-        
-        return ans;
-        
+        return currString;
     }
 };
