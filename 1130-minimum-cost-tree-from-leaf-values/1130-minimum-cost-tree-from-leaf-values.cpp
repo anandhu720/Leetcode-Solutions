dd67ee44ba320c36af4266009c9f731e82d873e5
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-05-31 16:57:30 +0530

    Time: 6 ms (67.07%), Space: 8.4 MB (67.79%) - LeetHub

--- 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
+++ 1130-minimum-cost-tree-from-leaf-values/1130-minimum-cost-tree-from-leaf-values.cpp
@@ -1,31 +1,20 @@
 class Solution {
 public:
-    pair<int,int> dp[41][41];
     int mctFromLeafValues(vector<int>& arr) {
-        memset(dp,-1,sizeof(dp));
-       return dfs(arr,0,arr.size()-1).second; 
-    }
-    
-    pair<int,int> dfs(vector<int> &nums,int l,int r) {
         
-        if(l>r) return {0,100000};
+        int sum = 0;
         
-        if(l == r) return {nums[l],0};
-        
-        if(dp[l][r].first != -1) return dp[l][r];
-        
-        pair<int,int> ans = {0,100000};
-        for(int i=l;i<r;i++) {
-            auto left_p = dfs(nums,l,i);
-            auto right_p = dfs(nums,i+1,r);
+        while(arr.size() > 1) {
+            int minIndex = min_element(arr.begin(),arr.end()) - arr.begin();
             
-            int total = left_p.second+right_p.second+(left_p.first*right_p.first);
-            if(total < ans.second) {
-                ans.first = max(left_p.first,right_p.first);
-                ans.second = total;
-            }
+            int left = minIndex-1 >= 0 ? arr[minIndex-1] : INT_MAX;
+            int right = minIndex+1 < arr.size() ? arr[minIndex+1] : INT_MAX;
+            
+            sum += arr[minIndex] * min(left,right);
+            
+            arr.erase(arr.begin()+minIndex);
         }
         
-        return dp[l][r] = ans;
+        return sum;
     }
 };
