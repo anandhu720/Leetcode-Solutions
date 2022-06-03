0c731079602e9584497670e658c97415662a4ba1
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-06-03 10:51:19 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (80.41%) - LeetHub

--- 365-water-and-jug-problem/365-water-and-jug-problem.cpp
+++ 365-water-and-jug-problem/365-water-and-jug-problem.cpp
@@ -7,19 +7,6 @@ public:
     }
     int gcd(int a, int b)
     {
-        // Everything divides 0
-        if (a == 0)
-            return b;
-        if (b == 0)
-            return a;
-  
-        // base case
-        if (a == b)
-            return a;
-  
-        // a is greater
-        if (a > b)
-            return gcd(a-b, b);
-        return gcd(a, b-a);
+        return b == 0 ? a : gcd(b, a % b);   
     }
 };
