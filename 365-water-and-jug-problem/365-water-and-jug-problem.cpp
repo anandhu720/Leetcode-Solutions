863062e58283ffdb909298e3160e1325c43061d5
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-06-03 10:51:59 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (80.41%) - LeetHub

--- 365-water-and-jug-problem/365-water-and-jug-problem.cpp
+++ 365-water-and-jug-problem/365-water-and-jug-problem.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     bool canMeasureWater(int jug1, int jug2, int target) {
-        int x = gcd(jug1,jug2);
-        
-        return target%x == 0 and target <= jug1+jug2;
+        return target%gcd(jug1,jug2) == 0 and target <= jug1+jug2;
     }
     int gcd(int a, int b)
     {
