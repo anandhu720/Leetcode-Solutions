b414a03898e95426097be4ed1d0b7b18f6fff24a
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-06-04 09:50:43 +0530

    Time: 72 ms (94.43%), Space: 27.3 MB (95.63%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,35 +1,25 @@
-class Solution {
+class Solution { // 48 ms, faster than 99.64%
 public:
-    
-    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int grad[] = {1,0,-1,0,1};
-        int m = mat.size(),n = mat[0].size();
-        queue<pair<int,int>> q;
-        
-        for(int i=0;i<m;i++) {
-            for(int j=0;j<n;j++) {
-                if(mat[i][j] == 0) q.push({i,j});
-                else mat[i][j] = -1;
+    vector<vector<int>> updateMatrix(vector<vector<int>> &mat) {
+        int m = mat.size(), n = mat[0].size(), INF = m + n; 
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (mat[r][c] == 0) continue;
+                int top = INF, left = INF;
+                if (r - 1 >= 0) top = mat[r - 1][c];
+                if (c - 1 >= 0) left = mat[r][c - 1];
+                mat[r][c] = min(top, left) + 1;
             }
         }
-        
-        while(!q.empty()) {
-            auto x = q.front(); q.pop();
-            int r = x.first;
-            int c = x.second;
-            
-            for(int k=0;k<4;k++) {
-                int row = r + grad[k];
-                int col = c + grad[k+1];
-                if(row<0 or col<0 or row>=m or col>=n or mat[row][col] != -1) continue;
-                
-                mat[row][col] = mat[r][c] + 1;
-                q.push({row,col});
+        for (int r = m - 1; r >= 0; r--) {
+            for (int c = n - 1; c >= 0; c--) {
+                if (mat[r][c] == 0) continue;
+                int bottom = INF, right = INF;
+                if (r + 1 < m) bottom = mat[r + 1][c];
+                if (c + 1 < n) right = mat[r][c + 1];
+                mat[r][c] = min(mat[r][c], min(bottom, right) + 1);
             }
         }
-        
-        
         return mat;
     }
-
 };
