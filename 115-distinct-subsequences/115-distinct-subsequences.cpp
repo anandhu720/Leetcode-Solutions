d346886c542123d3273e448eb0040d5e8c65707d
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-06-14 11:07:57 +0530

    Time: 52 ms (53.57%), Space: 18.5 MB (45.98%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -2,21 +2,21 @@ class Solution {
 public:
     int numDistinct(string s, string t) {
         vector<vector<int>> dp(s.size(),vector<int>(t.size(),-1));
-        return dfs(0,0,s,t,dp);
+        return dfs(s.size()-1,t.size()-1,s,t,dp);
     }
     
     int dfs(int i,int j,string &s,string &t,vector<vector<int>> &dp) {
         
-        if(j == t.size()) return 1;
-        if(i == s.size()) return 0;
+        if(j < 0) return 1;
+        if(i < 0) return 0;
         
         if(dp[i][j] != -1) return dp[i][j];
         
         int match = 0,notMatch = 0;
         if(s[i] == t[j]) {
-            match = dfs(i+1,j+1,s,t,dp) + dfs(i+1,j,s,t,dp);
+            match = dfs(i-1,j-1,s,t,dp) + dfs(i-1,j,s,t,dp);
         }else{
-            notMatch = dfs(i+1,j,s,t,dp);
+            notMatch = dfs(i-1,j,s,t,dp);
         }
         
         return dp[i][j] = match + notMatch;
