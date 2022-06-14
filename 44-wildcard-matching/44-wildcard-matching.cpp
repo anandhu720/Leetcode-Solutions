773dd10f16564ac52c65c73b3d793bcae5d9405c
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-06-14 22:27:46 +0530

    Time: 99 ms (42.07%), Space: 11.3 MB (49.19%) - LeetHub

--- 44-wildcard-matching/44-wildcard-matching.cpp
+++ 44-wildcard-matching/44-wildcard-matching.cpp
@@ -1,8 +1,33 @@
 class Solution {
 public:
     bool isMatch(string s, string p) {
-        vector<vector<int>> dp(s.length(),vector<int> (p.length(),-1));
-        return dfs(s.length()-1,p.length()-1,s,p,dp);
+        int m = s.length(),n = p.length();
+        vector<vector<bool>> dp(m+1,vector<bool> (n+1,false));
+        
+        dp[0][0] = true;
+        
+        for(int i=1;i<=n;i++) {
+            bool flag = true;
+            for(int ii=1;ii<=i;ii++) 
+                if(p[ii-1] != '*') {
+                    flag = false;
+                    break;
+                }
+            dp[0][i] = flag;
+        }
+        
+        for(int i=1;i<=m;i++) {
+            for(int j=1;j<=n;j++) {
+                if(s[i-1] == p[j-1] or p[j-1] == '?')
+                    dp[i][j] = dp[i-1][j-1];
+                else if(p[j-1] == '*')
+                    dp[i][j] = (dp[i-1][j] or dp[i][j-1]);
+                else
+                    dp[i][j] = false;
+            }
+        }
+        
+        return dp[m][n];       
     }
     
     bool dfs(int i,int j,string &s,string &p,vector<vector<int>> &dp) {
