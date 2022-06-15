c4fe64ff0563fc51ea589bd9d35cf7bb7c88b44e
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-06-15 15:21:35 +0530

    Time: 15 ms (57.42%), Space: 13.2 MB (24.62%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,30 +1,27 @@
 class Solution {
 public:
-    int maxProfit(int k, vector<int>& prices) {
-        vector<vector<vector<int>>> dp(prices.size(),
-                                    vector<vector<int>> (2,vector<int>(k+1,-1)) 
+    int maxProfit(int time, vector<int>& prices) {
+        vector<vector<vector<int>>> dp(prices.size()+1,
+                                    vector<vector<int>> (2,vector<int>(time+1,0)) 
                                      );
-        return dfs(0,prices,1,k,dp);
-    }
-    
-    int dfs(int index,vector<int> &prices,int buy,int k,vector<vector<vector<int>>> &dp) {
         
-        if(index == prices.size() or k == 0) return 0;
+        for(int index=prices.size()-1;index >= 0;index--) {
+            for(int buy=0;buy<2;buy++) {
+                for(int k=1;k <= time;k++) {
+                    
+                    int price = 0;
+                    if(buy == 1) 
+                        price = max(-prices[index] + dp[index+1][0][k],dp[index+1][1][k]);
+                    else
+                        price = max(prices[index] + dp[index+1][1][k-1],dp[index+1][0][k]);     
+                    
+                    dp[index][buy][k] = price;
+                }
+            }
+        }
         
-        if(dp[index][buy][k] != -1) return dp[index][buy][k];
+        return dp[0][1][time];
         
-        int price = 0;
-        if(buy == 1) 
-            price = max(
-                        -prices[index] + dfs(index+1,prices,0,k,dp),
-                        dfs(index+1,prices,1,k,dp)
-                    );
-        else
-            price = max(
-                        prices[index] + dfs(index+1,prices,1,k-1,dp),
-                        dfs(index+1,prices,0,k,dp)
-                    );
         
-        return dp[index][buy][k] = price;
     }
 };
