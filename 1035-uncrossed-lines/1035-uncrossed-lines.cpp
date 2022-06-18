2662e8d9b82c58573eeb5125301b3bbe804beba4
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-06-18 11:50:49 +0530

    Time: 30 ms (35.44%), Space: 13 MB (26.73%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.cpp
+++ 1035-uncrossed-lines/1035-uncrossed-lines.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
-        vector<vector<int>> dp(nums1.size(),vector<int>(nums2.size(),-1));
-        return dfs(0,0,nums1,nums2,dp);
-    }
-    
-    int dfs(int i,int j,vector<int> &s,vector<int> &p,vector<vector<int>> &dp) {
+        vector<vector<int>> dp(nums1.size()+1,vector<int>(nums2.size()+1,0));
         
-        if(i == s.size() or j == p.size()) return 0;
+        for(int i=nums1.size()-1;i>=0;i--) {
+            for(int j=nums2.size()-1;j>=0;j--) {  
+                if(nums1[i] == nums2[j])  
+                    dp[i][j] = 1 + dp[i+1][j+1];
+                else
+                    dp[i][j] = max(dp[i+1][j],dp[i][j+1]);
+            }
+        }
         
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        if(s[i] == p[j]) return dp[i][j] = 1 + dfs(i+1,j+1,s,p,dp);
-        
-        return dp[i][j] = max(dfs(i+1,j,s,p,dp),dfs(i,j+1,s,p,dp));
+        return dp[0][0];
     }
 };
