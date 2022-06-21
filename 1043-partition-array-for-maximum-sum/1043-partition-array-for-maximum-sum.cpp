3ac8ef86e6ead9c08198e1c751ed0fac4e43b1e3
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-06-21 11:15:59 +0530

    Time: 31 ms (33.11%), Space: 8.6 MB (33.02%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    int maxSumAfterPartitioning(vector<int>& arr, int k) {
-        vector<int> dp(arr.size(),-1);
-        return dfs(0,arr,k,dp);
-    }
-    
-    int dfs(int index,vector<int> &nums,int k,vector<int> &dp) {
+    int maxSumAfterPartitioning(vector<int>& nums, int k) {
+        vector<int> dp(nums.size()+1,0);
         
-        if(index >= nums.size()) return 0;
-        
-        if(dp[index] != -1) return dp[index];
-        
-        int cut = index;
-        int maxi = INT_MIN;
-        int x = nums[index];
-        while(cut < nums.size() and cut < index+k) {
+        for(int index=nums.size()-1;index>=0;index--) {
+            int cut = index;
+            int maxi = INT_MIN;
+            int x = nums[index];
+            while(cut < nums.size() and cut < index+k) {
+                x = max(x,nums[cut]);
+                int ans = x*(cut-index+1) + dp[cut+1];
+                cut = cut+1;
+                maxi = max(maxi,ans);
             
-            x = max(x,nums[cut]);
-            int ans = x*(cut-index+1) + dfs(cut+1,nums,k,dp);
-            cut = cut+1;
-            maxi = max(maxi,ans);
-            
+            }
+            dp[index] = maxi;
         }
         
-        return dp[index] = maxi;
+        return dp[0];
+        
     }
 };
