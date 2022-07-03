179795b0e6cffcf8027e1617d7d4269268df7277
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-07-03 12:42:21 +0530

    Added solution - LeetHub

---
+++ 4. Kth largest element - GFG/4.-kth-largest-element.cpp
@@ -0,0 +1,41 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to return kth largest element from an array.
+int KthLargest(int arr[], int n, int k) {
+       priority_queue<int> pq(arr, arr+n);
+       while(--k){
+           pq.pop();
+       }
+       return pq.top();
+   }
+};
+
+// { Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+
+        int arr[n];
+        for (int i = 0; i < n; i++) cin >> arr[i];
+        Solution ob;
+
+        cout << ob.KthLargest(arr, n, k) << endl;
+    }
+
+    return 0;
+}  // } Driver Code Ends
