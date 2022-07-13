e16a3eedb8cf1e5625eed5e2c9f46dab2e3ff840
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-07-13 22:30:07 +0530

    Time: 14 ms (39.91%), Space: 8.1 MB (86.49%) - LeetHub

--- 419-battleships-in-a-board/419-battleships-in-a-board.cpp
+++ 419-battleships-in-a-board/419-battleships-in-a-board.cpp
@@ -1,14 +1,4 @@
 class Solution {
-private:
-    void dfs(int r,int c,vector<vector<char>> &board) {
-        if(r >= board.size() or c >= board[r].size() or r < 0 or c < 0 or board[r][c] == '.')
-            return;
-        if(board[r][c] == 'X') board[r][c] = '.';
-        dfs(r+1,c,board);
-        dfs(r-1,c,board);
-        dfs(r,c+1,board);
-        dfs(r,c-1,board);
-    }
 public:
     int countBattleships(vector<vector<char>>& board) {
         int m = board.size(),n = board[0].size();
@@ -16,10 +6,8 @@ public:
         
         for(int i=0;i<m;i++) {
             for(int j=0;j<n;j++) {
-                if(board[i][j] == 'X') {
-                    count += 1;
-                    dfs(i,j,board);
-                }
+                count += board[i][j] == 'X' and 
+                         (i == 0 or board[i-1][j] != 'X') and (j == 0 or board[i][j-1] != 'X');
             }
         }
         
