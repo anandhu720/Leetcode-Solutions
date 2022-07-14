76a753530025d8c15dc6e2bf26fd1ff43bfbcbcc
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-07-14 19:45:51 +0530

    Time: 34 ms (89.07%), Space: 14.1 MB (71.39%) - LeetHub

--- 690-employee-importance/690-employee-importance.cpp
+++ 690-employee-importance/690-employee-importance.cpp
@@ -10,24 +10,23 @@ public:
 
 class Solution {
 private:
-    void dfs(vector<Employee*> &emp,int id,int &res) {
+    void dfs(unordered_map<int,Employee*> &umap,int id,int &res) {
         
-        for(auto it : emp) {
-            if(it->id == id) {
-                res += it->importance;
+        Employee *it = umap[id];
+
+        res += it->importance;
                 
-                for(auto i : it->subordinates) {
-                    dfs(emp,i,res);
-                }
-                
-                break;
-            }
+        for(auto i : it->subordinates) {
+            dfs(umap,i,res);
         }
+                
     }
 public:
     int getImportance(vector<Employee*> emp, int id) {
         int res = 0;
-        dfs(emp,id,res);
+        unordered_map<int,Employee*> umap;
+        for(auto it : emp) umap[it->id] = it;
+        dfs(umap,id,res);
         return res;
     }
 };
