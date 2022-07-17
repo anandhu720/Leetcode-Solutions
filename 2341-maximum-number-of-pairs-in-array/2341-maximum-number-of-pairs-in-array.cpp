41c544e8d8ff37eb64eddbc94c0bf2c7eb06a365
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-07-17 10:18:45 +0530

    Time: 4 ms (50.00%), Space: 9.3 MB (100.00%) - LeetHub

--- 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.cpp
+++ 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
+    // Time = O(nlogn) Space = O(1)
     vector<int> numberOfPairs(vector<int>& nums) {
                 
-        sort(nums.begin(),nums.end());
-        
+        unordered_map<int,int> umap;        
+        for(auto it : nums) umap[it]++;
         vector<int> ans(2,0);
-        int i;
-        for(i=1;i<nums.size();) {
-            if(nums[i-1] == nums[i]) {
-                ans[0]++;
-                i += 2;
+        for(auto it : umap) {
+            if(it.second%2 != 0) {
+                ans[0] += (it.second-1)/2;
+                ans[1] += 1;
             }else{
-                ans[1]++;
-                i += 1;
+                ans[0] += it.second/2;
             }
         }
-        
-        if(i == nums.size()) ans[1]++;
         
         return ans;
     }
