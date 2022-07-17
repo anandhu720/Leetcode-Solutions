467062e333b613710585442e261ac8c4a5389f84
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-07-17 11:03:01 +0530

    Time: 340 ms (100.00%), Space: 60 MB (100.00%) - LeetHub

--- 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -3,40 +3,28 @@ public:
     int maximumSum(vector<int>& nums) {
         int maxSum = -1;
         
-        unordered_map<int,vector<int>> umap;
+        unordered_map<int,int> umap;
         
         for(auto it : nums) {
+            
             int currNum = it;
             int currSum = 0;
+            
             while(currNum) {
                 currSum += currNum%10;
                 currNum /= 10;
             }
             
-            if(umap.find(currSum) == umap.end()) {
-                umap[currSum].push_back(it);
+            if(umap.find(currSum) != umap.end()) {
+                maxSum = max(maxSum,it+umap[currSum]);
+                umap[currSum] = max(umap[currSum],it);
             }else{
-                if(umap[currSum].size() == 1) {
-                    umap[currSum].push_back(it);
-                    if(umap[currSum][0] < umap[currSum][1])
-                        swap(umap[currSum][0],umap[currSum][1]);
-                }else{
-                    if(umap[currSum][1] < it) umap[currSum][1] = it;
-                    else if(umap[currSum][0] < it) umap[currSum][0] = it;
-                    if(umap[currSum][0] < umap[currSum][1])
-                        swap(umap[currSum][0],umap[currSum][1]);
-                }
+                umap[currSum] = it;
             }
-        }
-        
-        for(auto it : umap) {
-            if(it.second.size() < 2) continue;
-            int localSum = 0;
-            localSum += it.second[0] + it.second[1];
             
-            maxSum = max(maxSum,localSum);
         }
         
         return maxSum;
+
     }
 };
