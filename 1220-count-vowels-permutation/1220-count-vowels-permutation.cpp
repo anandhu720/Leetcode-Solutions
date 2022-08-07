4a00e32fb86a8211edc4dd08db10210b231aebfb
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-08-07 17:22:13 +0530

    Time: 888 ms (5.14%), Space: 104.6 MB (6.85%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -2,30 +2,31 @@ class Solution {
 public:
     int m = 1e9 + 7;
     int countVowelPermutation(int n) {
-        vector<char> s = {'a','e','i','o','u'};
+        unordered_map<char,vector<char>> umap;
+        
+        umap['{'] = {'a','e','i','o','u'};
+        umap['a'] = {'e'};
+        umap['e'] = {'a','i'};
+        umap['i'] = {'a','e','o','u'};
+        umap['o'] = {'i','u'};
+        umap['u'] = {'a'};
         char prev = '{';
         vector<vector<int>> dp(n+1,vector<int>(27,-1));
-        return dfs(n,s,prev,dp);
+        return dfs(n,umap,prev,dp);
     }
     
-    int dfs(int n,vector<char> &s,char prev,vector<vector<int>> &dp) {
+    int dfs(int n,unordered_map<char,vector<char>> &umap,char prev,vector<vector<int>> &dp) {
         
         if(n == 0) return 1;
         
         if(dp[n][prev - 'a'] != -1) return dp[n][prev - 'a'];
         
         long long ans = 0;
-        for(int i=0;i<s.size();i++) {
             
-            if(prev == '{') ans += dfs(n-1,s,s[i],dp)%m;
-            else if(prev == 'a' and s[i] == 'e') ans += dfs(n-1,s,s[i],dp)%m;
-            else if(prev == 'e' and (s[i] == 'a' or s[i] == 'i')) ans += dfs(n-1,s,s[i],dp)%m;
-            else if(prev == 'i' and s[i] != 'i') ans += dfs(n-1,s,s[i],dp)%m;
-            else if(prev == 'o' and (s[i] == 'i' or s[i] == 'u')) ans += dfs(n-1,s,s[i],dp)%m;
-            else if(prev == 'u' and s[i] == 'a') ans += dfs(n-1,s,s[i],dp)%m;
-            
-        }
+        vector<char> s = umap[prev];
         
+        for(auto it : s) ans += dfs(n-1,umap,it,dp)%m;
+
         return dp[n][prev - 'a'] = ans%m;
     }
 };
