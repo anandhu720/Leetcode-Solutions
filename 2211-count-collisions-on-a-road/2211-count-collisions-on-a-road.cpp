1efb86f377c5d33d99a217ce269ff0904dd9065d
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-08-16 20:53:23 +0530

    Time: 67 ms (78.87%), Space: 16.4 MB (46.10%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.cpp
@@ -1,33 +1,22 @@
 class Solution {
 public:
     int countCollisions(string dir) {
-        stack<char> st;
-        st.push(dir[0]);
+        int carsFromRight = 0;
+        int n = dir.size();
+        int res = 0;
         
-        int no = 0;
+        int i = 0;
+        while(i < n and dir[i] == 'L') i++;
         
-        for(int i=1;i<dir.size();i++) {
-            while(!st.empty() and st.top() != dir[i]) {
-                pair<char,int> ans = check(st.top(),dir[i]);
-                dir[i] = ans.first;
-                no += ans.second;
-                st.pop();
+        for(;i<n;i++) {
+            if(dir[i] == 'R') carsFromRight++;
+            else{
+                res += (dir[i] == 'S') ? carsFromRight : carsFromRight + 1;
+                carsFromRight = 0;
             }
-            
-            st.push(dir[i]);
         }
         
-        return no;
-    }
-    
-    pair<char,int> check(char s,char t) {
-        if(s == 'R' and t == 'L')
-            return {'S',2};
-        else if(s == 'R' and t == 'S')
-            return {'S',1};
-        else if(s == 'S' and t == 'L')
-            return {'S',1};
-        
-        return {t,0};
+        return res;
     }
 };
+
