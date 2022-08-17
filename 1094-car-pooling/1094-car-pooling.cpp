8ae6910b8a4a69b3e593d2806a58c07d01105462
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-08-17 21:47:05 +0530

    Time: 25 ms (47.32%), Space: 11 MB (21.30%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -1,44 +1,18 @@
 class Solution {
 public:
     bool carPooling(vector<vector<int>>& trips, int capacity) {
-        vector<vector<int>> nums;
+        map<int,int> umap;
         
         for(auto it : trips) {
-            vector<int> tmp;
-            tmp.push_back(it[1]);
-            tmp.push_back(it[2]);
-            tmp.push_back(it[0]);
-            nums.push_back(tmp);
+            umap[it[1]] += it[0];
+            umap[it[2]] -= it[0];
         }
         
-        sort(nums.begin(),nums.end());
-        
-    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        int currCap = 0;
-        
-        for(int i=0;i<nums.size();i++) {
-            while(pq.size() > 0 and nums[i][0] >= pq.top().first) {
-                currCap -= pq.top().second;
-                pq.pop();
-            }
-            
-            currCap += nums[i][2];
-            pq.push({nums[i][1],nums[i][2]});
-            
-            
-            if(currCap > capacity) return false;
+        for(auto it : umap) {
+            capacity -= it.second;
+            if(capacity < 0) return false;
         }
         
         return true;
-        
     }
 };
-
-
-/*
-
- [[1,5,2],[3,3,7]]
- 
- {end,start,value}
-
-*/
