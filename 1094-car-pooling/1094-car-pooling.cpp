54e84a3b40c050b27ba07aba2dbe5e4ed6b905c2
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-08-17 21:41:33 +0530

    Time: 41 ms (18.57%), Space: 12.2 MB (9.60%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -13,33 +13,18 @@ public:
         
         sort(nums.begin(),nums.end());
         
-        stack<vector<int>> s;
+    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        int currCap = 0;
         
-        s.push(nums[0]);
-        
-        int currCap = nums[0][2];
-        if(currCap > capacity) return false;
-        for(int i=1;i<nums.size();i++) {
-            while(!s.empty() and nums[i][0] >= s.top()[1]) {
-                vector<int> x = s.top();
-                s.pop();
-                currCap -= x[2];
+        for(int i=0;i<nums.size();i++) {
+            while(pq.size() > 0 and nums[i][0] >= pq.top().first) {
+                currCap -= pq.top().second;
+                pq.pop();
             }
             
             currCap += nums[i][2];
-            vector<vector<int>> f;
-            while(!s.empty() and nums[i][1] > s.top()[1]) {
-                auto x = s.top();
-                f.push_back(x);
-                s.pop();
-            }
+            pq.push({nums[i][1],nums[i][2]});
             
-            if(f.size() > 0) {
-                s.push(nums[i]);
-                for(int i = f.size()-1;i>=0;i--) s.push(f[i]);
-            }else{
-                s.push(nums[i]);
-            }
             
             if(currCap > capacity) return false;
         }
@@ -53,5 +38,7 @@ public:
 /*
 
  [[1,5,2],[3,3,7]]
+ 
+ {end,start,value}
 
 */
