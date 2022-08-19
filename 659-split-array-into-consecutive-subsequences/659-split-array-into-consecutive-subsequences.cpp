378ebc76e058b57495bbe7fe0e3bb48fc39a90b1
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-08-19 22:32:34 +0530

    Time: 161 ms (53.13%), Space: 58.4 MB (58.44%) - LeetHub

--- 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
+++ 659-split-array-into-consecutive-subsequences/659-split-array-into-consecutive-subsequences.cpp
@@ -3,26 +3,22 @@ public:
     bool isPossible(vector<int>& nums) {
         unordered_map<int,int> left,end;
         
-        for(auto ele : nums) left[ele]++;
+        for(auto it : nums) left[it]++;
         
-        for(auto ele : nums) {
-            if(left[ele] == 0) continue; // no element left
-            left[ele]--; // going to use that element
+        for(auto i : nums) {
+            if(left[i] == 0) continue;
+            left[i]--;
             
-            if(end[ele - 1] > 0) {
-                
-                end[ele-1]--;
-                end[ele]++;
-                
-            }else if(left[ele+1] > 0 and left[ele+2] > 0) {
-                
-                left[ele+1]--;
-                left[ele+2]--;
-                end[ele+2]++;
-                
-            }else 
+            if(end[i-1] > 0) {
+                end[i-1]--;
+                end[i]++;
+            }else if(left[i+1] > 0 and left[i+2] > 0) {
+                left[i+1]--;
+                left[i+2]--;
+                end[i+2]++;
+            }else{
                 return false;
-            
+            }
         }
         
         return true;
