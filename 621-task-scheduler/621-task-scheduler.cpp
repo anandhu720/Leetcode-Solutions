4a258317864a262a6238c6f9413455a1fd984a52
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-08-24 21:13:28 +0530

    Time: 123 ms (57.34%), Space: 38.1 MB (29.31%) - LeetHub

--- 621-task-scheduler/621-task-scheduler.cpp
+++ 621-task-scheduler/621-task-scheduler.cpp
@@ -1,44 +1,38 @@
 class Solution {
 public:
-    // Using max heap and queue
-    // Time = O(t.size() * m) m = n
-    //here n = [0,100] ,so we can avoid it
-    int leastInterval(vector<char>& t, int n) {
+    int leastInterval(vector<char>& tasks, int n) {
         unordered_map<char,int> umap;
-        for(auto it : t) umap[it]++;
-        vector<int> v;
-        for(auto it : umap) v.emplace_back(it.second);
+        for(auto it : tasks) umap[it]++;
         
-        make_heap(v.begin(),v.end());
+        priority_queue<int> pq;
+        for(auto it : umap) pq.push(it.second);
         
-        int time = 0;
+        queue<pair<int,int>> q; // no,time
         
-        queue<pair<int,int>> q; // {no , time}
+        long long time = 0;
         
-        while(v.size() != 0 or !q.empty()) {
+        while(pq.size() > 0 or !q.empty()) {
             
             time += 1;
             
-            if(v.size() != 0) {
-                int cnt = v.front() - 1;
-                pop_heap(v.begin(),v.end());
-                v.pop_back();
-                if(cnt) {
-                    q.push({cnt,time+n});
-                }
+            if(pq.size() > 0) {
+                int x = pq.top(); pq.pop();
+                if(x-1 > 0)
+                    q.push({x-1,time+n});
             }
             
             if(!q.empty()) {
-                auto it = q.front();
-                if(it.second == time) {
+                auto x = q.front();
+                if(x.second == time) {
                     q.pop();
-                    v.push_back(it.first);
-                    push_heap(v.begin(),v.end());
+                    pq.push(x.first);
                 }
             }
             
         }
         
         return time;
+        
+        
     }
 };
