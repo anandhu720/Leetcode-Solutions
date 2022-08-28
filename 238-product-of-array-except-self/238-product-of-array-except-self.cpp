3ddcc3677c610b1bba7cfdc5aff76a017a70566b
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-08-28 15:00:28 +0530

    Time: 59 ms (9.48%), Space: 24.1 MB (52.11%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -1,20 +1,52 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        
-        int first = 1,last = 1;
-        
-        vector<int> ans(nums.size(),1);
-        for(int i=0;i<n;i++) {
-            
-            ans[i] *= first;  
-            first *= nums[i];
-            
-            ans[n-1-i] *= last;
-            last *= nums[n-1-i];
+        vector<int> ans(nums.size(),0);
+        int noOfZeros = 0;
+        int neg = 0;
+        for(auto it : nums) {
+            if(it < 0) neg++;
+            if(it == 0) noOfZeros++;
+        }
+        if(noOfZeros > 1) return ans;
+        else if(noOfZeros == 1) {
+            return tmp(nums);
         }
         
+        long long prod = 1;
+        for(auto it : nums) prod *= it;
+        
+        prod = abs(prod);
+        
+        for(int i=0;i<nums.size();i++) {
+            int tmp = prod / abs(nums[i]);
+            if(neg%2 == 0) {
+                if(nums[i] < 0) ans[i] = -1*tmp;
+                else ans[i] = tmp;
+            }else{
+                if(nums[i] < 0) ans[i] = tmp;
+                else ans[i] = -1*tmp;
+            }
+        }
+        
+        return ans;
+    }
+    
+    vector<int> tmp(vector<int> &nums) {
+        vector<int> ans(nums.size(),0);
+        
+        int index = 0;
+        int product = 1;
+        for(int i=0;i<nums.size();i++) {
+            if(nums[i] == 0) {
+                index = i;
+                continue;
+            }
+            
+            product *= nums[i];
+        }
+        
+        ans[index] = product;
         return ans;
     }
 };
