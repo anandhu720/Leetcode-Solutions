ac4f299b0c69546b85315ad270cdecde49e5dddc
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-08-28 14:10:52 +0530

    Time: 178 ms (60.44%), Space: 25.4 MB (54.45%) - LeetHub

--- 301-remove-invalid-parentheses/301-remove-invalid-parentheses.cpp
+++ 301-remove-invalid-parentheses/301-remove-invalid-parentheses.cpp
@@ -1,49 +1,63 @@
 class Solution {
+private:
+    vector<string> ans;
+    unordered_map<string,bool> umap;
+    // T.C. : O(n) S.C. : O(n)
+    int check(string &s) {
+        stack<char> st;
+        int ans = 0;
+        for(int i=0;i<s.length();i++) {
+            
+            if(!st.empty() and s[i] == ')') {
+                while(!st.empty() and st.top() >= 'a' and st.top() <= 'z') st.pop();
+                if(!st.empty() and st.top() == '(') {
+                    st.pop();
+                    continue;
+                }
+            }
+            
+            if(s[i] == ')') ans++;
+            else if(s[i] == '(' or (s[i] >= 'a' and s[i] <= 'z')) st.push(s[i]);
+            
+        }
+        
+        while(!st.empty()) {
+            if(st.top() == '(' or st.top() == ')') ans++;
+            st.pop();
+        }
+        
+        return ans;
+    }
+    
+    void dfs(string s,int mini) {
+        
+        if(umap.find(s) != umap.end()) return;
+        else umap[s] = true;
+
+        
+        
+        if(mini == 0) {
+            int flag = check(s);
+            if(flag == 0) {
+                ans.push_back(s);
+            }
+            return;
+        }
+        
+        for(int i=0;i<s.length();i++) {
+            if(s[i] != '(' and s[i] != ')') continue;
+            
+            dfs(s.substr(0,i)+s.substr(i+1),mini-1);
+        }
+        
+    }
+    
 public:
+    vector<string> removeInvalidParentheses(string s) {
+        int mini = check(s);
+        
+        dfs(s,mini);
 
-	/*Functtion to calculate miismatched bracket*/
-	int minvalid(string s){
-		stack<int> st;
-		for(auto it: s){
-			if(it == '(')   st.push(it);
-			else if(it == ')'){
-				if(!st.empty() && st.top() == '(')
-					st.pop();
-				else    st.push(it);
-			}
-		}
-		return st.size();
-	}
-	
-	
-	void solve(string s, vector<string>& ans, map<string, bool>& mpp, int removal){
-		/*If the string is already present in the map return back*/
-		if(mpp[s])  return;
-		
-		/*If the string is not prensent in the map add it, and give the value of the string TRUE*/
-		else    mpp[s] = true;
-
-		if(removal==0){
-			/*Even though removal becomes zero we still have to cjeck for its validity*/
-			int n = minvalid(s);
-			if(n == 0)
-				ans.push_back(s);
-			return;
-		}
-		
-		for(int i=0;i<s.length();i++){
-			string left = s.substr(0,i);
-			string right = s.substr(i+1);
-			/*Excluding the ith element*/
-			solve(left+right, ans, mpp, removal-1); 
-		}
-	}
-
-	vector<string> removeInvalidParentheses(string s) {
-		int removal = minvalid(s);
-		vector<string> ans;
-		map<string, bool> mpp;
-		solve(s, ans, mpp, removal);
-		return ans;
-	}
+        return ans;
+    }
 };
