5b8930d84767f5dbb126a399a8f43da10296e756
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-08-29 21:58:46 +0530

    Time: 4 ms (67.67%), Space: 11.9 MB (67.44%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -19,7 +19,8 @@ public:
     
     void dfs(TreeNode *root,int level,vector<int> &ans) {
         if(!root) return;
-        if(ans.size() == level) ans.push_back(root->val);
+        if(level == ans.size()) ans.push_back(root->val);
+        
         dfs(root->right,level+1,ans);
         dfs(root->left,level+1,ans);
     }
