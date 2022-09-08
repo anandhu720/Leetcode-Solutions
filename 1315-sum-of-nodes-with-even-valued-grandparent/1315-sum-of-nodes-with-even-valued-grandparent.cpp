6a0fcf1ac431917b6a0b6d00b5bfa436ca283c40
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-09-08 21:20:34 +0530

    Time: 83 ms (40.91%), Space: 41.5 MB (44.11%) - LeetHub

--- 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
+++ 1315-sum-of-nodes-with-even-valued-grandparent/1315-sum-of-nodes-with-even-valued-grandparent.cpp
@@ -12,21 +12,12 @@
 class Solution {
 public:
     int sumEvenGrandparent(TreeNode* root) {
-        if(!root) return 0;
-        
-        int sum = 0;
-        if(root->val%2 == 0) {
-            if(root->left) {
-                if(root->left->left) sum += root->left->left->val;
-                if(root->left->right) sum += root->left->right->val;
-            }
-            if(root->right) {
-                if(root->right->left) sum += root->right->left->val;
-                if(root->right->right) sum += root->right->right->val;
-            }            
-        }
-        
-        return sum + sumEvenGrandparent(root->left) + sumEvenGrandparent(root->right);
+        return dfs(root);
+    }
+    
+    int dfs(TreeNode *root,int p = 1,int gp = 1) {
+        return root ? 
+dfs(root->left,root->val,p) + dfs(root->right,root->val,p) + (gp%2 == 0 ? root->val : 0) : 0;
     }
 
 };
