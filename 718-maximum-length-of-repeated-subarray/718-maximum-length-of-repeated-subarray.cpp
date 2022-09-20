ef895fd5899c0fdeb85da0d8f8b670d61f565712
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-09-20 17:41:29 +0530

    Time: 2110 ms (5.01%), Space: 109.4 MB (5.11%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -1,20 +1,27 @@
 class Solution {
-public:
-    int findLength(vector<int>& nums1, vector<int>& nums2) {
-        vector<vector<int>> dp(nums1.size()+1,vector<int> (nums2.size()+1,0));
-        int maxLength = 0;
+private:
+    int lcs(vector<int>& n1, vector<int>& n2, int i, int j, int &ans, vector<vector<int>>&dp){
+        if (i == n1.size() or j == n2.size()) return 0; 
         
-        for(int i=1;i<dp.size();i++) {
-            for(int j=1;j<dp[0].size();j++) {
-                
-                if(nums1[i-1] == nums2[j-1]) {
-                    dp[i][j] = dp[i-1][j-1] + 1;
-                    maxLength = max(maxLength,dp[i][j]);
-                }   
-            }
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        int cur = 0;
+        if (n1[i] == n2[j]){
+            cur = lcs(n1, n2, i + 1, j + 1, ans, dp)+1; 
         }
         
+        lcs(n1, n2, i, j + 1, ans, dp);
+        lcs(n1, n2, i + 1, j, ans, dp);
         
-        return maxLength;
+        ans = max(ans, cur);
+        return dp[i][j] = cur;
+    } 
+public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int n = nums1.size(), m = nums2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        int ans = 0;
+        lcs(nums1, nums2, 0, 0, ans, dp);
+        return ans;
     }
 };
