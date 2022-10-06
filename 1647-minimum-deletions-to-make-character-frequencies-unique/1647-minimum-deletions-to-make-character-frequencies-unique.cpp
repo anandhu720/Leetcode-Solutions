a8fac320826d6cf2c023e631c684386ab3f4ff3a
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-10-06 19:34:24 +0530

    Time: 151 ms (40.67%), Space: 17.3 MB (58.54%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -2,12 +2,22 @@ class Solution {
 public:
     int minDeletions(string s) {
         int cnt[26] = {}, res = 0;
-        unordered_set<int> used;
+        unordered_set<int> uset;
         for (char ch : s)
             ++cnt[ch - 'a'];
-        for (int i = 0; i < 26; ++i)
-            for (; cnt[i] > 0 && !used.insert(cnt[i]).second; --cnt[i])
-                ++res;
+
+        for (int i = 0; i < 26; ++i) {
+            if(uset.find(cnt[i]) != uset.end()) {
+                while(cnt[i] != 0 and uset.find(cnt[i]) != uset.end()) {
+                    res++;
+                    cnt[i]--;
+                }
+            }
+            uset.insert(cnt[i]);
+
+        }
+        
         return res;
     }
 };
+
