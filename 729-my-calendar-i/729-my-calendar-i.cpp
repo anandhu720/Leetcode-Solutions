76231d11d6bed18a41f715622a0d3124572ffc25
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-10-07 21:42:40 +0530

    Time: 406 ms (10.56%), Space: 44.1 MB (5.90%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,55 +1,27 @@
-class SGTree {
-public:
-    int start = 0,end = 0;
-    SGTree *left = NULL,*right = NULL;
-    
-    SGTree(int x,int y) {
-        start = x;
-        end = y; 
-    }
-};
 class MyCalendar {
 public:
-    SGTree *root;
+    map<int,int> umap;
     MyCalendar() {
-        root = new SGTree(0,0);
-    }
-    
-    bool checkBook(SGTree *root,int target_s,int target_e) {
-        
-        int start = root->start;
-        int end = root->end;
-        
-        if(target_e <= start) {
-            
-            if(root->left == NULL) {
-                SGTree *tmp = new SGTree(target_s,target_e);
-                root->left = tmp;
-                return true;
-            }
-            
-            return checkBook(root->left,target_s,target_e);
-            
-        }
-        
-        if(target_s >= end) {
-            if(root->right == NULL) {
-                SGTree *tmp = new SGTree(target_s,target_e);
-                root->right = tmp;
-                return true;
-            }
-            
-            return checkBook(root->right,target_s,target_e);
-            
-        }
-        
-        return false;
         
     }
     
     bool book(int start, int end) {
-        SGTree *p = root;
-        return checkBook(p,start,end);
+        umap[start]++;
+        umap[end]--;
+        
+        int overlap = 0;
+        
+        for(auto it : umap) {
+            overlap += it.second;
+            
+            if(overlap > 1) {
+                umap[start]--;
+                umap[end]++;
+                return false;
+            }
+        }
+        
+        return true;
     }
 };
 
