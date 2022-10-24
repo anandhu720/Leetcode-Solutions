51b68875e583201986242963f3c5d9748a41cda3
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-10-24 16:15:15 +0530

    Time: 16 ms (64.50%), Space: 10.5 MB (55.75%) - LeetHub

---
+++ 0841-keys-and-rooms/0841-keys-and-rooms.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void dfs(int node,vector<bool> &vis,vector<vector<int>> &rooms) {
+        
+        vis[node] = true;
+        
+        for(auto it : rooms[node]) {
+            if(vis[it] == false)
+                dfs(it,vis,rooms);
+        }
+    }
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        vector<bool> vis(rooms.size(),false);
+        
+        dfs(0,vis,rooms);
+        
+        for(auto it : vis) 
+            if(it == false) return false;
+        return true;
+    }
+};
