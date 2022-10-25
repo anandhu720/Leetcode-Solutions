701406bd02d2282a1238ae85edb716436e657bec
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-10-25 22:31:25 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,34 +3,97 @@
 using namespace std;
 
 // } Driver Code Ends
+
+class DisjointSet
+{
+
+    vector<int> rank, parent, size;
+
+public:
+    DisjointSet(int n)
+    {
+        rank.resize(n + 1, 0);
+        size.resize(n + 1, 0);
+        for (int i = 0; i <= n; i++)
+            parent.push_back(i);
+    }
+
+    // find ultimate parent
+    int findUPar(int node)
+    {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]); // doing path comp
+    }
+
+    void unionByRank(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        if (ulp_u == ulp_v)
+            return; // they belong to same component
+
+        if (rank[ulp_u] < rank[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+        }
+        else if (rank[ulp_v] < rank[ulp_u])
+        {
+            parent[ulp_v] = ulp_u;
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            rank[ulp_u]++;
+        }
+    }
+
+    void unionBySize(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        if (ulp_u == ulp_v)
+            return; // they belong to same component
+
+        if (size[ulp_u] < size[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
 class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int V, vector<vector<int>> umap[])
     {
-        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
+        DisjointSet ds(V);
+        vector<vector<int>> nums;
         
-        vector<int> vis(V,0);
-        
-        pq.push({0,0,-1}); // wt,node,parent
-        int sum = 0;
-        
-        while(!pq.empty()) {
-            int wt = pq.top()[0];
-            int node = pq.top()[1];
-            int parent = pq.top()[2];
-            pq.pop();
-            
-            if(vis[node] == 1) continue;
-            
-            sum += wt;
-            vis[node] = 1;
-            
-            for(auto it : umap[node]) {
-                if(vis[it[0]] == 0) 
-                    pq.push({it[1],it[0],node});
+        for(int i=0;i<V;i++) {
+            for(auto it : umap[i]) {
+                nums.push_back({it[1],i,it[0]});
             }
+        }
+        
+        sort(nums.begin(),nums.end());
+        
+        int sum = 0;
+        for(auto it : nums) {
+            if(ds.findUPar(it[1]) == ds.findUPar(it[2])) continue;
+            
+            sum += it[0];
+            
+            ds.unionByRank(it[1],it[2]);
         }
         
         return sum;
