5691a4db491a09c512ee5f722f9bee30636aad16
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-10-25 16:44:02 +0530

    Time: 87 ms (85.72%), Space: 16.7 MB (15.25%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,39 +1,54 @@
 class Solution {
 public:
-    //Floyd Warshall Algo
-    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<int>> matrix(n,vector<int>(n,1e9));
+    //Dijkstra Algorithm
+    unordered_map<int,vector<pair<int,int>>> umap;
+    void dijkstra(int src,vector<int> &dist) {
         
-        for(auto it : edges) {
-            matrix[it[0]][it[1]] = it[2];
-            matrix[it[1]][it[0]] = it[2];
-        }
+        dist[src] = 0;
         
-        for(int i=0;i<n;i++) matrix[i][i] = 0;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
         
-        for(int k=0;k<n;k++) {
-            for(int i=0;i<n;i++) {
-                for(int j=0;j<n;j++) {
-                    matrix[i][j] = min(matrix[i][j],matrix[i][k] + matrix[k][j]);
+        pq.push({0,src}); //dist,node
+        
+        while(!pq.empty()) {
+            int node = pq.top().second;
+            int d = pq.top().first;
+            pq.pop();
+            
+            for(auto it : umap[node]) {
+                int child = it.first;
+                int childd = it.second;
+                
+                if(d + childd < dist[child]) {
+                    dist[child] = d + childd;
+                    pq.push({dist[child],child});
                 }
             }
+        }
+    }
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        for(auto it : edges) {
+            umap[it[0]].push_back({it[1],it[2]});
+            umap[it[1]].push_back({it[0],it[2]});
         }
         
         int cnt = 0;
         int currMin = INT_MAX;
         
         for(int i=0;i<n;i++) {
-            int currCount = 0;
-            for(int j=0;j<n;j++) {
-                if(matrix[i][j] <= distanceThreshold) currCount++;
+            vector<int> dist(n,INT_MAX);
+            dijkstra(i,dist);
+            int c = 0;
+            for(auto it : dist) {
+                if(it <= distanceThreshold) c++;
             }
-            if(currCount <= currMin) {
+            
+            if(c <= currMin) {
                 cnt = i;
-                currMin = currCount;
+                currMin = c;
             }
         }
         
         return cnt;
-        
     }
 };
