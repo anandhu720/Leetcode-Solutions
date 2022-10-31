b77e03c980c195691cb2e3bbc70e8a1b8cab27b1
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-10-31 18:51:02 +0530

    Time: 540 ms (14.37%), Space: 74.5 MB (10.98%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.cpp
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -1,95 +1,38 @@
-class DisjointSet
-{
-
-    vector<int> rank, parent, size;
-
-public:
-    DisjointSet(int n)
-    {
-        rank.resize(n + 1, 0);
-        size.resize(n + 1, 0);
-        for (int i = 0; i <= n; i++)
-            parent.push_back(i);
-    }
-
-    // find ultimate parent
-    int findUPar(int node)
-    {
-        if (node == parent[node])
-            return node;
-        return parent[node] = findUPar(parent[node]); // doing path comp
-    }
-
-    void unionByRange(int u, int v)
-    {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-
-        if (ulp_u == ulp_v)
-            return; // they belong to same component
-
-        if (rank[ulp_u] < rank[ulp_v])
-        {
-            parent[ulp_u] = ulp_v;
-        }
-        else if (rank[ulp_v] < rank[ulp_u])
-        {
-            parent[ulp_v] = ulp_u;
-        }
-        else
-        {
-            parent[ulp_v] = ulp_u;
-            rank[ulp_u]++;
-        }
-    }
-
-    void unionBySize(int u, int v)
-    {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-
-        if (ulp_u == ulp_v)
-            return; // they belong to same component
-
-        if (size[ulp_u] < size[ulp_v])
-        {
-            parent[ulp_u] = ulp_v;
-            size[ulp_v] += size[ulp_u];
-        }
-        else
-        {
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v];
-        }
-    }
-};
-
 class Solution {
+private:
+    unordered_map<int,vector<int>> umap;
+    vector<int> color;
 public:
-    bool possibleBipartition(int n, vector<vector<int>>& nums) {
-        DisjointSet ds(n);
-        vector<vector<int>> adj(n);
+    bool dfs(int node) {
         
-        for(auto it : nums) {
-            adj[it[0] - 1].push_back(it[1] - 1);
-            adj[it[1] - 1].push_back(it[0] - 1);
-        }
-        
-        for(auto it : adj) {
-            if(it.size() == 0) continue;
-            int curr = it[0];
-            for(auto i : it) {
-                ds.unionByRange(curr,i);
+        for(auto it : umap[node]) {
+            if(color[it] == -1) {
+                color[it] = 1 - color[node];
+                if(!dfs(it)) return false;
+            }else if(color[it] == color[node]){
+                return false;
             }
         }
         
+        return true;
+    }
+    bool possibleBipartition(int n, vector<vector<int>>& nums) {
+        
         for(auto it : nums) {
-            if(ds.findUPar(it[0]-1) == ds.findUPar(it[1] - 1))
-                return false;
+            umap[it[0]].push_back(it[1]);
+            umap[it[1]].push_back(it[0]);
+        }
+        
+        color.resize(n+1,-1);
+        
+        for(int i=1;i<=n;i++) {
+            if(color[i] != -1) continue;
+            
+            color[i] = 1;
+            if(!dfs(i)) return false;
         }
         
         return true;
-        
         
         
     }
