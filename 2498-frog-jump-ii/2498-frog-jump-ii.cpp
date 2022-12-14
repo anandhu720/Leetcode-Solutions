1d6afff8c92da3b5b5588555c24a190cd9e8651f
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-12-14 11:12:54 +0530

    Time: 145 ms (97.03%), Space: 65.4 MB (29.88%) - LeetHub

--- 2498-frog-jump-ii/2498-frog-jump-ii.cpp
+++ 2498-frog-jump-ii/2498-frog-jump-ii.cpp
@@ -1,57 +1,12 @@
 class Solution {
 public:
-    int dfs(int mid,vector<int> &nums) {
-        vector<int> tmp = nums;
-        unordered_set<int> vis;
+    int maxJump(vector<int>& stones) {
+        int ans = stones[1] - stones[0];
         
-        int prev = 0;
-        int i = 1;
-        for(i=1;i<tmp.size();i++) {
-            
-            if(tmp[i] - tmp[prev] <= mid) {
-                continue;
-            }
-            if(i-1 == prev) return false;
-            prev = i-1;
-            vis.insert(i-1);
-            i--;
+        for(int i=2;i<stones.size();i++) {
+            ans = max(ans,stones[i] - stones[i-2]);
         }
         
-        if(i != tmp.size()) return false;
-        
-        
-        prev = nums.size()-1;
-        for(i=tmp.size()-2;i>=0;i--) {
-            if(vis.count(i) > 0) continue;
-            if(tmp[prev] - tmp[i] <= mid) {
-                continue;
-            }
-            int j = i+1;
-            while(vis.count(j) > 0) j++;
-            if(j >= prev) return false;
-            prev = j;
-            vis.insert(j);
-            i++;
-        }
-        if(i >= 0) return false;
-        
-        return true;
-    }
-    int maxJump(vector<int>& nums) {
-        int left = 0,right = nums[nums.size()-1] - nums[0];
-        int res = right;
-        
-        while(left <= right) {
-            int mid = left + (right - left)/2;
-            // cout<<mid<<endl;
-            if(dfs(mid,nums)) {
-                res = mid;
-                right = mid - 1;
-            }else{
-                left = mid + 1;
-            }
-        }
-        
-        return res;
+        return ans;
     }
 };
