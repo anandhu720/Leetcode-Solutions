3e2adbd130d3db304ffc8d29af4bdbf84a189634
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2022-12-14 10:36:23 +0530

    Time: 3 ms (56.21%), Space: 7.8 MB (25.79%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
-    int dfs(int index,vector<int> &nums,vector<int> &dp) {
-        
-        if(index >= nums.size()) return 0;
-        if(dp[index] != -1) return dp[index];
-        
-        int rob = nums[index] + dfs(index+2,nums,dp);
-        int leave = dfs(index+1,nums,dp);
-        
-        return dp[index] = max(rob,leave);
-    }
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size(),-1);
-        return dfs(0,nums,dp);
+        vector<int> dp(nums.size()+2,0);
+        
+        for(int i=nums.size()-1;i>=0;i--){
+            dp[i] = max(dp[i+1],nums[i] + dp[i+2]);
+        }
+        
+        return dp[0];
     }
 };
