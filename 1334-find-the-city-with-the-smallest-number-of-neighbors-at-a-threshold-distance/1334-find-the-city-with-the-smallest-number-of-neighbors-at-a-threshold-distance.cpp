d76669e975056cb14fed44a66661193eb8dee992
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2023-01-10 12:17:32 +0530

    Time: 93 ms (81.78%), Space: 16.7 MB (10.68%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,54 +1,54 @@
 class Solution {
-public:
-    //Dijkstra Algorithm
+private:
     unordered_map<int,vector<pair<int,int>>> umap;
-    void dijkstra(int src,vector<int> &dist) {
-        
-        dist[src] = 0;
+    
+    void dijkstra(int node,vector<int> &dist) {
         
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        
-        pq.push({0,src}); //dist,node
+        pq.push({0,node}); //dist,node
+        dist[node] = 0;
         
         while(!pq.empty()) {
-            int node = pq.top().second;
-            int d = pq.top().first;
-            pq.pop();
+            pair<int,int> x = pq.top(); pq.pop();
+            int d = x.first;
+            int node = x.second;
             
             for(auto it : umap[node]) {
                 int child = it.first;
-                int childd = it.second;
-                
-                if(d + childd < dist[child]) {
-                    dist[child] = d + childd;
-                    pq.push({dist[child],child});
+                int val = it.second;
+                if(d+val < dist[child]) {
+                    dist[child] = d+val;
+                    pq.push({d+val,child});
                 }
             }
         }
     }
+    
+public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        
         for(auto it : edges) {
             umap[it[0]].push_back({it[1],it[2]});
             umap[it[1]].push_back({it[0],it[2]});
         }
         
-        int cnt = 0;
-        int currMin = INT_MAX;
+        int ans = 0,c = INT_MAX;
         
         for(int i=0;i<n;i++) {
             vector<int> dist(n,INT_MAX);
             dijkstra(i,dist);
-            int c = 0;
+            
+            int cnt = 0;
             for(auto it : dist) {
-                if(it <= distanceThreshold) c++;
+                if(it <= distanceThreshold) cnt++;
             }
             
-            if(c <= currMin) {
-                cnt = i;
-                currMin = c;
+            if(cnt <= c) {
+                ans = i;
+                c = cnt;
             }
         }
         
-        return cnt;
+        return ans;
     }
 };
