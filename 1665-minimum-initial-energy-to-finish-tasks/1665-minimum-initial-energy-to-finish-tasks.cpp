27aeaf014b716d9bf7021a5b0100156953174491
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2023-01-11 22:42:59 +0530

    Time: 2601 ms (5.92%), Space: 481.8 MB (6.58%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -1,35 +1,29 @@
 class Solution {
 public:
+    static bool comp(vector<int> &a,vector<int> &b) {
+        if((a[1] - a[0]) > (b[1] - b[0])) return true;
+        return false;
+    }
     bool dfs(int mid,vector<vector<int>> &nums) {
-        
-        for(int i=0;i<nums.size();i++) {
-            if(mid < nums[i][1]) return false;
-            mid -= nums[i][0];
+        for(auto it : nums) {
+            if(mid < it[1]) return false;
+            mid -= it[0];
         }
         return mid >= 0;
     }
-    int minimumEffort(vector<vector<int>>& nums) {
-        sort(nums.begin(),nums.end(),[&](vector<int> &p,vector<int> &q){
-            if(p[1]-p[0] > q[1]-q[0])
-                return true;
-            return false;
-        });
-        
-        int left = 1,right = 0;
-        for(auto &it : nums) right += it[1];
-        
-        int res = INT_MAX;
+    int minimumEffort(vector<vector<int>>& tasks) {
+        sort(tasks.begin(),tasks.end(),comp);
+        int left = 0,right = 1e9;
+        int ans = right;
         while(left <= right) {
             int mid = left + (right - left)/2;
-            
-            if(dfs(mid,nums)) {
-                res = mid;
+            if(dfs(mid,tasks)) {
+                ans = mid;
                 right = mid - 1;
             }else{
                 left = mid + 1;
             }
         }
-        
-        return res;
+        return ans;
     }
 };
