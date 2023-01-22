559f9bdec7f60167d7f31ebaf22733cfeb35eadd
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2023-01-22 22:46:11 +0530

    Time: 1304 ms (50.00%), Space: 385.4 MB (33.33%) - LeetHub

--- 2547-minimum-cost-to-split-an-array/2547-minimum-cost-to-split-an-array.cpp
+++ 2547-minimum-cost-to-split-an-array/2547-minimum-cost-to-split-an-array.cpp
@@ -1,33 +1,43 @@
 class Solution {
 private:
     vector<int> dp;
-    int dfs(int index,vector<int> &nums,int k) {
+    int dfs(int index,vector<int> &nums,vector<vector<int>> &tval,int k) {
         if(index >= nums.size()) return 0;
-        unordered_map<int,int> uset;
-        int count = 0;
-        
+
         if(dp[index] != -1) return dp[index];
         
         int ans = INT_MAX;
         for(int i=index;i<nums.size();i++) {
-                      
-            uset[nums[i]]++;
             
-            if(uset[nums[i]] > 2) count++;
-            else if(uset[nums[i]] == 2) count+=2;
+            int next = dfs(i+1,nums,tval,k);
             
-
-            int next = dfs(i+1,nums,k);
-            
-            ans = min(ans,count+k+next);
+            ans = min(ans,tval[index][i]+k+next);
             
         }
         return dp[index] = ans;
     }
 public:
     int minCost(vector<int>& nums, int k) {
+        vector<vector<int>> tval(1001,vector<int>(1001,0));
+        
+        for(int i=0;i<nums.size();i++) {
+            vector<int> tmp(1001,0);
+            int cnt = 0;
+            
+            for(int j=i;j<nums.size();j++) {
+                tmp[nums[j]]++;
+                
+                if(tmp[nums[j]] == 2) cnt+=2;
+                else if(tmp[nums[j]] > 2) cnt++;
+                
+                tval[i][j] = cnt;
+            }
+            
+        }
+        
+        
         dp.resize(nums.size(),-1);
-        return dfs(0,nums,k);
+        return dfs(0,nums,tval,k);
     }
 };
 
