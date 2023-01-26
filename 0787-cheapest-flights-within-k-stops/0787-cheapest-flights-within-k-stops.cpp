aa081c3b60da3089a03d8eef7696c4fa75674f30
Anandha Krishnan <70640349+anandhu720@users.noreply.github.com>
2023-01-26 12:26:37 +0530

    Time: 55 ms (28.83%), Space: 16.8 MB (13.64%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,42 +1,30 @@
 class Solution {
-public:
-    // Dijkstra's Algorithm
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int d, int k) {
-        unordered_map<int,vector<pair<int,int>>> adj;
+private:
+    unordered_map<int,vector<pair<int,int>>> umap;
+    vector<vector<int>> dp;
+    int dfs(int src,int d,int k) {
         
-        for(auto it : flights) {
-            adj[it[0]].push_back({it[1],it[2]});
+        if(src == d) return 0;
+        if(k == 0) return 1e9;
+        
+        if(dp[src][k] != -1) return dp[src][k];
+        
+        int ans = 1e9;
+        for(auto it : umap[src]) {
+            ans = min(ans,it.second + dfs(it.first,d,k-1));
         }
         
-        vector<int> stops(n+1,INT_MAX);
-        
-        // dist,node,stops
-        priority_queue <vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
-        
-        pq.push({0,src,0});
-        
-        while(!pq.empty()) {
-            auto it = pq.top(); pq.pop();
-            
-            int dst = it[0];
-            int node = it[1];
-            int stop = it[2];
-            
-            if(stop > stops[node] or stop > k+1) continue;
-            stops[node] = stop;
-            
-            if(node == d) return dst; 
-            
-            for(auto child : adj[node]) {
-                int nextDist = child.second;
-                int nextNode = child.first;
-                
-                pq.push({dst+nextDist,nextNode,stop+1});
-            }
-        }
-        
-        return -1;
-        
+        return dp[src][k] = ans;
     }
-
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        for(auto it : flights) {
+            umap[it[0]].push_back({it[1],it[2]});
+        }
+        
+        dp.resize(101,vector<int>(101,-1));
+        int ans = dfs(src,dst,k+1);
+        
+        return ans >= 1e9 ? -1 : ans;
+    }
 };
